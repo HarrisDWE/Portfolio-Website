4745c08a40913f20ebf4fb229a28441da720ed99
HarrisDWE <47992021+HarrisDWE@users.noreply.github.com>
2021-07-09 10:44:20 +0100

    Began to implement reading project data from a a JSON file
    Started to implement reading the project card data from a JSON file and creating a unique project card from that data.
    
    Currently it creates all of the necessary elements, but the contents are all undefined.
    
    I am also cheating with this as the two methods written (one commented), aren't actually reading from a JSON, they are simply reading from a "fake" JSON file (really just a JS file with an object), and a JS object in the style of a JSON.
    
    Actually reading from a local JSON file will require either node.js or JQuery.

--- Scripts/ProjectCardCreator.js
+++ Scripts/ProjectCardCreator.js
@@ -1,4 +1,27 @@
-window.addEventListener("DOMContentLoaded", createElements);
+window.addEventListener("DOMContentLoaded", createProjects);
+
+function createProjects() {
+    //Loading from "JSON" test - need to change how it is loaded, it is currently effective a JS document with a var
+    //let projectsJSON = JSON.parse(projectsData);
+    //This is for testing and it doesn't work anyway
+    let projectsData = '{ "projects" : [' +
+        '{ "name" : "Testing JSON"},' + 
+        '{ "description" : "this is an example description" },' + 
+        '{ "tech" : "currently just text, but will change to images later" },' +
+        '{ "imageSrc" : "Test-Image.png" },' +
+        '{ "imageAlt" : "This is alt text" },' +
+        '{ "linkText" : "Example Link" },' +
+        '{ "linkSrc" : "https://www.youtube.com/watch?v=dQw4w9WgXcQ" } ]}';
+
+    const projectsJSON = JSON.parse(projectsData);
+
+    projectPageElements = createElements();
+        
+    addContent(projectsJSON, projectPageElements[0], projectPageElements[1], projectPageElements[2], projectPageElements[3]);
+    addToDOM(projectPageElements[4]);
+    
+}
+
 
 function createElements() {
     //Define key variables
@@ -40,22 +63,33 @@ function createElements() {
     cardDesc.appendChild(projectDesc);
     projectLinksP.appendChild(projectLinks);
 
-    addContent(projectName, projectLinks, projectDesc, projectImage);
-    addToDOM(cardContainer);
+    return [projectName, projectDesc, projectImage, projectLinks, cardContainer];
 }
 
 
-function addContent(projectName, projectLinks, projectDesc, projectImage) {
-    let projectNameText = document.createTextNode("Example Project Title (Using JS)");
-    let projectDescText = document.createTextNode("Example description (using JS). Bacon ipsum dolor amet chicken turducken porchetta pork loin t-bone strip steak. Filet mignon brisket beef strip steak prosciutto venison. Hamburger tail filet mignon strip steak tri-tip short ribs jerky doner bacon burgdoggen swine capicola porchetta ham hock. Shankle pork loin ham turducken frankfurter pig pancetta short ribs tail ground round. Shankle beef ribs flank kielbasa shank picanha. Tenderloin corned beef pork loin, hamburger kielbasa kevin fatback pork chop. T-bone boudin pork chislic ground round beef ribs.");
+function addContent(project, projectName, projectDesc, projectImage, projectLinks) {
+    //Hardcoded test
+    // let projectNameText = document.createTextNode("Example Project Title (Using JS)");
+    // let projectDescText = document.createTextNode("Example description (using JS). Bacon ipsum dolor amet chicken turducken porchetta pork loin t-bone strip steak. Filet mignon brisket beef strip steak prosciutto venison. Hamburger tail filet mignon strip steak tri-tip short ribs jerky doner bacon burgdoggen swine capicola porchetta ham hock. Shankle pork loin ham turducken frankfurter pig pancetta short ribs tail ground round. Shankle beef ribs flank kielbasa shank picanha. Tenderloin corned beef pork loin, hamburger kielbasa kevin fatback pork chop. T-bone boudin pork chislic ground round beef ribs.");
 
-    projectLinks.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    projectLinks.innerText = "link";
+    // projectLinks.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    // projectLinks.innerText = "link";
+
+    // projectName.appendChild(projectNameText);
+    // projectDesc.appendChild(projectDescText);
+    // projectImage.src = "../Images/Test-Image.png";
+    // projectImage.alt = "test image";
+
+    let projectNameText = document.createTextNode(project.name);
+    let projectDescText = document.createTextNode(project.description);
+
+    projectLinks.href = project.linkSrc;
+    projectLinks.innerText = project.linkText;
 
     projectName.appendChild(projectNameText);
     projectDesc.appendChild(projectDescText);
-    projectImage.src = "../Images/Test-Image.png";
-    projectImage.alt = "test image";
+    //projectImage.src = project.imageSrc; //This doesn't work - can't get the image - probably something do with with not being able to get local files
+    projectImage.alt = project.imageAlt;
 }
 
 
