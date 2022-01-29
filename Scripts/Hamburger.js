4f3b1788276d1816156471210516e6ab1d8e1001
HarrisDWE <47992021+HarrisDWE@users.noreply.github.com>
2022-01-29 15:26:33 +0000

    Setup an in-progress page

--- Scripts/Hamburger.js
+++
@@ -1,7 +0,0 @@
-function hamburgerControl() {
-    let links = document.getElementById("burger-dropdown-container");
-    if (links.style.display === "block") 
-        links.style.display = "none"; 
-    else 
-        links.style.display = "block";
-}
